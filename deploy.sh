14215d72d7b4c50e4eb5be6fa13259d864415fd9
千迦 <qianjia.work@gmail.com>
2022-05-14 23:39:55 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,13 +17,13 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
-git push -f git@github.com:qianjia-world/<USERNAME>.github.io.git master
+# git push -f git@github.com:qianjia-world/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 # 以這個專案來講就要改成這樣以下這樣，下面是走 ssh 模式
 # git push -f git@github.com:hsiangfeng/HexfootMusic.git master:gh-pages
 # 除此之外，也可以改走 HTTPS 模式
-# git push -f https://github.com/hsiangfeng/HexfootMusic.git master:gh-pages
+git push -f https://github.com/qianjia-world/test.git master:gh-pages
 
 cd -
